a9b502017646a25c439a343161696074ee65ef97
benauld <ben@portobello-consultancy.co.uk>
2020-02-28 12:25:01 +0000

    transition changing colors

--- src/gameJquery.js
+++ src/gameJquery.js
@@ -8,14 +8,18 @@ $(document).ready(function() {
 		$(this).css('color', 'white');
 		var winner = game.takeTurn(turn, parseInt($(this).attr('id')));
 		if (winner === true) {
-			$('.winner').text('We have a winner !');
+			$('.winner').text('We have a winner!');
 			$('.winner').css('color', 'red');
 			$('.button').attr('disabled', true);
 		}
 		if (turn === 'X') {
 			turn = 'O';
+			$('#player2').css('color', 'darkred');
+			$('#player1').css('color', 'black');
 		} else {
 			turn = 'X';
+			$('#player1').css('color', 'darkred');
+			$('#player2').css('color', 'black');
 		}
 	});
 
